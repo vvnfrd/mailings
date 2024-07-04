8e1d9b15977adf5470e6fae2a05cf6521a29234e
zetmarley <zzzetmarley@gmail.com>
2024-07-04 18:15:01 +0700

    Manager Interface #2

--- main/templatetags/has_group.py
+++ main/templatetags/has_group.py
@@ -2,6 +2,15 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+
+@register.filter
+def media_filter(path):
+    if path:
+        return f'/media/{path}'
+    else:
+        return '#'
